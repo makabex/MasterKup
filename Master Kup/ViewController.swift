45c893cd7f9ba11dc94c638d7e36b20b2f74aabd
makabex <dennis_pham@hotmail.com>
2016-07-27 18:14:07 -0700

    Initial Commit

---
+++ Master Kup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Master Kup
+//
+//  Created by Dennis Pham on 2016-07-27.
+//  Copyright © 2016 Dennis Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
