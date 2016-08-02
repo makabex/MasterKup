3912f25027a80e658438a848091ec13887b30d0c
makabex <dennis_pham@hotmail.com>
2016-08-02 12:22:09 -0700

    added photos

---
+++ Master Kup/View.swift
@@ -0,0 +1,24 @@
+//
+//  View.swift
+//  Master Kup
+//
+//  Created by Dennis Pham on 2016-07-27.
+//  Copyright © 2016 Dennis Pham. All rights reserved.
+//
+
+import UIKit
+
+class View: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
